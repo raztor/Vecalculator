fedb2e59b629d6c97f7253b4606617fe94327419
Benjamin Munoz (Raztorr) <contacto@webizy.cl>
2022-09-26 23:04:19 -0300

    initial project structure creation and updated .gitignore to avoid pushing Cmake related stuff (except CMakeLists.txt)

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+
+
+int main(){
+
+
+
+
+    return 0;
+}
