7945a64b192c72085ebfc537c718c7f6fa84e110
andreavalfonzo <andreavalfonzo13@gmail.com>
2022-09-27 19:10:51 +0000

    añadidura función norma

--- src/main.cpp
+++ src/main.cpp
@@ -40,8 +40,13 @@ float cross();
 float angle();
 float unit();*/
 double exit();
-double norm(){
-    
+double norm(vec a){
+    vec mag_2d, mag_3d;
+    mag_2d = sqrt(pow(eje_x.a, 2), pow(eje_y.a, 2));
+
+    mag_3d = sqrt(pow(eje_x.a, 2), pow(eje_y.a, 2), pow(eje_z.a,2));
+
+    return mag_2d, mag_3d;
 }
 
 
@@ -168,6 +173,13 @@ int main(){
             break;
         case 7:
             cout << "Norma" << endl;
+             vector_result = norm(vector1);
+            if (dimension == 1){
+                //cout << "El resultado es: " << vector_result.eje_x << "i " << vector_result.eje_y << "j" << endl;
+                cout << "El resultado es:" << "("<< vector_result.eje_x << "," << vector_result.eje_y << ")" << endl;
+            }
+            else if (dimension == 2){
+                cout << "El resultado es:" << "("<< vector_result.eje_x << "," << vector_result.eje_y << "," << vector_result.eje_z << ")" << endl;
             break;
         case 8:
             cout << "Angulo" << endl;
